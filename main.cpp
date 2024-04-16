70065835960d8440e7acd1fb1dce04aaad1c1091
Yang.LUO <992534544@qq.com>
2024-04-16 18:18:35 +0800

    Update

--- main.cpp
+++ main.cpp
@@ -2,8 +2,18 @@
 
 #include <QApplication>
 
+#include <QSimpleUpdater.h>
+
 int main(int argc, char *argv[])
 {
+    QString url = "https://github.com/thinkexist1989/TestUpdater/config/updates.json";
+
+    QSimpleUpdater::getInstance()->setNotifyOnUpdate(url, true);
+    QSimpleUpdater::getInstance()->setNotifyOnFinish(url, false);
+
+    QSimpleUpdater::getInstance()->checkForUpdates(url);
+
+
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
